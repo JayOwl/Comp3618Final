13d605b9d3b1b9efd4055f964dad608ebd73c8df
ericb-bcit <52718646+ericb-bcit@users.noreply.github.com>
2020-03-19 13:34:46 -0700

    basic filesystemwatcher implementation

--- FileIOClient/MainWindow.xaml.cs
+++ FileIOClient/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,8 +20,32 @@ namespace FileIOClient {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        private FileSystemWatcher watcher = new FileSystemWatcher();
+        private List<String> paths = new List<string>();
         public MainWindow() {
             InitializeComponent();
+            Loaded += OnLoad;
         }
+
+        private void OnLoad(object sender, RoutedEventArgs e) {
+            ThreadPool.SetMaxThreads(Environment.ProcessorCount, Environment.ProcessorCount * 2);
+            Thread.CurrentThread.Name = "UI Thread";
+
+            watcher.Path = "C:\\finaltest";
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size;
+            watcher.Changed += FswOnChanged;
+            watcher.Created += FswOnChanged;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void FswOnChanged(object source, FileSystemEventArgs e) {
+            //filesystemwatcher events can fire multiple times for the same file, so this checks for dupes before adding
+            if (!paths.Contains(e.FullPath)) { 
+                paths.Add(e.FullPath);
+                //System.Diagnostics.Debug.Print(e.FullPath + " added");
+            }
+        }
+
     }
 }
